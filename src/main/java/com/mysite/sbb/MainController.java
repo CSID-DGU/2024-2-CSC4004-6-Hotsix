714f153059f0c106e2fb4ce0b0ac1ab62f013c2b
YiBangWon <68014598+YiBangWon@users.noreply.github.com>
2024-11-21 14:21:33 +0900

    게시글  및 댓글 추천 기능, 앵커 기능, 마크다운 기능, 게시판 검색 기능 추가
    게시글  및 댓글 추천 기능, 앵커 기능, 마크다운 기능, 게시판 검색 기능 추가

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -2,19 +2,12 @@ package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
-	@GetMapping("/어디 갈래?")
-	@ResponseBody
-	public String index() {
-		return "데이트 코스 추천 플랫폼 \"어디 갈래?\"입니다!";
-	}
-
-	@GetMapping("/")
-	public String root() {
-		return "redirect:/question/list";
-	}
+    @GetMapping("/")
+    public String root() {
+        return "index"; // "index.html" 파일 이름에서 확장자를 제외하고 반환
+    }
 }
