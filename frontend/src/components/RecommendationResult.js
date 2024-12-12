c9065e97145eb9f44f871fe4fe414145b0f1a70e
Myeonghoonjin <audgns1947@dgu.ac.kr>
2024-12-13 00:11:13 +0900

    12.13

---
+++ frontend/src/components/RecommendationResult.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import '../styles/RecommendationResult.css';
+import { useNavigate } from 'react-router-dom';
+
+function RecommendationResult(){
+
+    return(
+
+    );
+}
